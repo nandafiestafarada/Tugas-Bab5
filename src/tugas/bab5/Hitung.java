4b910d520ace40aeabbcb0bed8906e18a4601e20
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 11:51:28 +0700

    method penyederhanaan (fix class jaket)

--- src/tugas/bab5/Hitung.java
+++ src/tugas/bab5/Hitung.java
@@ -24,3 +24,26 @@ public class Hitung {
         System.out.println("Hasil pembagian    = " + nilai);
         Sederhana(bil1, bil2);
     }
+    
+    private void Sederhana(int bil1, int bil2) {
+        System.out.println("*******Dalam Bentuk Pecahan*******");
+        System.out.println("hasil dari pecahan = " + bil1 + "/" + bil2);
+        int a = bil1;
+        while (true) {
+            if (bil1 % 2 == 0 && bil2 % 2 == 0) {
+                bil1 = bil1 / 2;
+                bil2 = bil2 / 2;
+            } else if (bil1 % 3 == 0 && bil2 % 3 == 0) {
+                bil1 = bil1 / 3;
+                bil2 = bil2 / 3;
+            } else {
+                break;
+            }
+            if (bil1 != a) {
+                System.out.println("Disederhanakan menjadi = " + bil1 + "/" + bil2);
+            } else {
+                System.out.println("Disederhanakan menjadi = " + bil1 + "/" + bil2 + "(tidak dapat disederhankan)");
+            }
+        }
+    }
+}
