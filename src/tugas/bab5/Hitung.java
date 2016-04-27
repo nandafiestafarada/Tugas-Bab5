c4d1a300d7eb7de935c41e81599bc5904867e319
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 11:18:33 +0700

    method prkalian dan pembagian

--- src/tugas/bab5/Hitung.java
+++ src/tugas/bab5/Hitung.java
@@ -11,3 +11,16 @@ public class Hitung {
         int nilai = bil1 - bil2;
         System.out.println("Hasil pengurangan  = " + nilai);
     }
+    
+    public void Perkalian(int bil1, int bil2) {
+        int nilai = bil1 * bil2;
+        System.out.println("Hasil perkalian    = " + nilai);
+    }
+
+    public void Pembagian(int bil1, int bil2) {
+        double a = bil1;
+        double b = bil2;
+        double nilai = a / b;
+        System.out.println("Hasil pembagian    = " + nilai);
+        Sederhana(bil1, bil2);
+    }
