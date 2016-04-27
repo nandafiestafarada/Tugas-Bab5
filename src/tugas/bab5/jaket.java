dd343b750aeae1ea650ad6cfc3d6691be1f9a314
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 13:19:48 +0700

    membuat variabel

--- src/tugas/bab5/jaket.java
+++ src/tugas/bab5/jaket.java
@@ -1,1 +1,10 @@
+package Jaket;
+
+public class Jaket {
+
+    private final int jaketA = 100000;
+    private final int jaketB = 125000;
+    private final int jaketC = 175000;
+    private static int jumlah = 0;
+    private static int A, B, C = 0;
 
