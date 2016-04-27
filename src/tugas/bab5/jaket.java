f7a1b2d933add19b2611808886ff3bd6fa0dc373
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 13:31:37 +0700

    konstruktor

--- src/tugas/bab5/jaket.java
+++ src/tugas/bab5/jaket.java
@@ -7,4 +7,14 @@ public class Jaket {
     private final int jaketC = 175000;
     private static int jumlah = 0;
     private static int A, B, C = 0;
+    
+    public Jaket(int banyak, int pilihan) {
+        if (pilihan == 1) {
+            A = banyak + A;
+        } else if (pilihan == 2) {
+            B = banyak + B;
+        } else {
+            C = banyak + C;
+        }
+    }
 
