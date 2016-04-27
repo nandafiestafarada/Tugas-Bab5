7da723f0c5f06d76fbe6f3b38f44bb6b10a77bd4
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 11:55:58 +0700

    membuat variabel

--- src/tugas/bab5/MainHitung.java
+++ src/tugas/bab5/MainHitung.java
@@ -1,1 +1,12 @@
+package Hitung;
 
+import java.util.Scanner;
+
+public class MainHitung {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Hitung h1 = new Hitung();
+        int bil1, bil2;
+        int pilihan;
+        
