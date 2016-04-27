770b8030942b266a872b6c2695876534abc7ce0a
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 12:26:02 +0700

    pembuatan menu

--- src/tugas/bab5/MainHitung.java
+++ src/tugas/bab5/MainHitung.java
@@ -9,4 +9,16 @@ public class MainHitung {
         Hitung h1 = new Hitung();
         int bil1, bil2;
         int pilihan;
+                
+        do {
+            System.out.println("**** MENU ****");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.println("5. Keluar");
+            System.out.println("**************");
+            System.out.print("Masukkan pilihan anda : ");
+            pilihan = in.nextInt();
+            System.out.println("_________________________");
         
