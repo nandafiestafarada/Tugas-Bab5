a2f267574a0f8f17580b911f8e69e205ec82efca
nandafiestafarada <nandafiestafarada@gmail.com>
2016-04-27 10:58:27 +0700

    method penjumlahan

--- src/tugas/bab5/Hitung.java
+++ src/tugas/bab5/Hitung.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab5;
+package Hitung;
 
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab5 {
+public class Hitung {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void Penjumlahan(int bil1, int bil2) {
+        int nilai = bil1 + bil2;
+        System.out.println("Hasil penjumlahan  = " + nilai);
     }
-    
-}
